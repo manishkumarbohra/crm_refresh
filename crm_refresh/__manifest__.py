a4fe8f2a165f56c050426a4877d50cff4b531eee
Manish Bohra <manishkumarbohra@outlook.com>
2025-04-02 20:01:02 +0530

    16.0 - module convert

--- crm_refresh/__manifest__.py
+++ crm_refresh/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'CRM Refresh',
-    'version': '17.0',
+    'version': '16.0',
     'summary': 'This module allows user to reload the CRM screen without refresh the webpage',
     'description': 'This module allows user to reload the CRM screen without refresh the webpage',
     'category': 'CRM',
